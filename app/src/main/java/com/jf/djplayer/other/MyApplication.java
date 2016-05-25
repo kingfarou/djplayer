93f8e57e731bec22c1573d01753a9a32900ec1f5
jf <60551112@qq.com>
2016-05-25 17:06:18 +0800

    添加一个全局打印方法

--- app/src/main/java/com/jf/djplayer/other/MyApplication.java
+++ app/src/main/java/com/jf/djplayer/other/MyApplication.java
@@ -2,6 +2,7 @@ package com.jf.djplayer.other;
 
 import android.app.Application;
 import android.content.Context;
+import android.util.Log;
 
 /**
  * Created by Administrator on 2015/7/20.
@@ -18,5 +19,14 @@ public class MyApplication extends Application {
     }
     public static Context getContext(){
         return context;
+    }
+
+    /**
+     * 这是一个全局打印方法，当该应用不再需要调试
+     * 只要注释这个方法里的代码即可，就能关闭所有打印
+     * @param info 要打印的调试信息
+     */
+    public static void printLog(String info){
+        Log.i("com.jf.djplayer.test", info);
     }
 }
