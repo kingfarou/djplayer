af510be90b598f2c5bb5895f0d0fdeb8c0d69e9f
jf <60551112@qq.com>
2016-06-07 16:34:47 +0800

    欢迎界面，去掉子线程的使用，改成透明度动画来实现过渡。

--- app/src/main/java/com/jf/djplayer/main/WelcomeActivity.java
+++ app/src/main/java/com/jf/djplayer/main/WelcomeActivity.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Toast;
 
 import com.jf.djplayer.R;
+import com.jf.djplayer.base.baseactivity.BaseActivity;
 import com.jf.djplayer.base.baseactivity.BaseNoTitleActivity;
 import com.jf.djplayer.other.MyApplication;
 import com.jf.djplayer.tool.FileTool;
@@ -18,25 +22,48 @@ import java.lang.ref.WeakReference;
  * Created by JF on 2016/3/6.
  * 欢迎界面-welcome page
  */
-public class WelcomeActivity extends BaseNoTitleActivity {
+public class WelcomeActivity extends BaseActivity {
 
-    private final StartMainActivityThread startMainActivityThread = new StartMainActivityThread(this);
+//    private final StartMainActivityThread startMainActivityThread = new StartMainActivityThread(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        View view = View.inflate(this, R.layout.activity_welcome, null);
+        setContentView(view);
+        //窗口界面的出现添加动画的效果
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0.2f, 1.0f);
+        alphaAnimation.setFillAfter(true);
+        alphaAnimation.setDuration(2000);
+        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                reDirectTo();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+        view.startAnimation(alphaAnimation);
+        appDirInit();
     }
 
     @Override
     protected int getContentViewId() {
-        return R.layout.activity_welcome;
+//        return R.layout.activity_welcome;
+        return -1;
     }
 
     @Override
     protected void initExtrasBeforeView() {
 //        使用异步任务来完成到"MainActivity"跳转
 //        new StartMainActivityThread().start();
-        startMainActivityThread.start();
+//        startMainActivityThread.start();
     }
 
     @Override
@@ -55,31 +82,36 @@ public class WelcomeActivity extends BaseNoTitleActivity {
         }
     }
 
+    //重定向到主界面去
+    private void reDirectTo(){
+        startActivity(new Intent(this, MainActivity.class));
+        finish();//启动完成关闭当前活动
+    }
 
-    private static class StartMainActivityThread extends Thread{
-
-        private final WeakReference<WelcomeActivity> activityWeakReference;
-
-        public StartMainActivityThread(WelcomeActivity welcomeActivity){
-            activityWeakReference = new WeakReference<>(welcomeActivity);
-        }
-
-        @Override
-        public void run() {
-            WelcomeActivity welcomeActivity = activityWeakReference.get();
-            if(welcomeActivity == null){
-                return;
-            }
-            //创建应用所需要的路径
-            welcomeActivity.appDirInit();
-            //两秒钟后启动主活动
-            try{
-                Thread.sleep(2000L);
-            }catch (InterruptedException e){
-                MyApplication.printLog("异常--位置--"+WelcomeActivity.class.getSimpleName());
-            }
-            welcomeActivity.startActivity(new Intent(welcomeActivity,MainActivity.class));
-            welcomeActivity.finish();//启动完成关闭当前活动
-        }
-    }//StartMainActivityThread
+//    private static class StartMainActivityThread extends Thread{
+//
+//        private final WeakReference<WelcomeActivity> activityWeakReference;
+//
+//        public StartMainActivityThread(WelcomeActivity welcomeActivity){
+//            activityWeakReference = new WeakReference<>(welcomeActivity);
+//        }
+//
+//        @Override
+//        public void run() {
+//            WelcomeActivity welcomeActivity = activityWeakReference.get();
+//            if(welcomeActivity == null){
+//                return;
+//            }
+//            //创建应用所需要的路径
+//            welcomeActivity.appDirInit();
+//            //两秒钟后启动主活动
+//            try{
+//                Thread.sleep(2000L);
+//            }catch (InterruptedException e){
+//                MyApplication.printLog("异常--位置--"+WelcomeActivity.class.getSimpleName());
+//            }
+//            welcomeActivity.startActivity(new Intent(welcomeActivity,MainActivity.class));
+//            welcomeActivity.finish();//启动完成关闭当前活动
+//        }
+//    }//StartMainActivityThread
 }
