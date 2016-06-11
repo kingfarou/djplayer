3758cc1925c27a746729219e3ae64f269eb629f2
jf <605511712@qq.com>
2016-06-11 20:16:23 +0800

    一些修补。

--- app/src/main/java/com/jf/djplayer/main/MainActivity.java
+++ app/src/main/java/com/jf/djplayer/main/MainActivity.java
@@ -97,8 +97,8 @@ public class MainActivity extends BaseNoActionBarActivity
     public void replaceFragments(Fragment fragment) {
         //更改"fragmentManager"里的"fragment"
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.setCustomAnimations(R.anim.main_fragment_translate_enter, R.anim.main_fragment_translate_exit,
-                R.anim.main_fragment_translate_pop_enter, R.anim.main_fragment_translate_pop_exit);
+        fragmentTransaction.setCustomAnimations(R.anim.slide_in_from_right, R.anim.slide_out_to_left,
+                R.anim.slide_in_from_left, R.anim.slide_out_to_right);
         fragmentTransaction.replace(R.id.fl_activity_main_fragment_container,fragment).addToBackStack(null).commit();
         //让堆栈理所保存的"fragment"与"FragmentManager"保持同步
 //        fragmentStacks.push(fragment.getClass().getSimpleName());
